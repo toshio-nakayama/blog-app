a1c28408916ab7308c525bdb58cee26775d9bd82
Toshio <toshio.h1990@gmail.com>
2023-05-01 10:05:28 +0900

    feat: Create Component

---
+++ components/footer.js
@@ -0,0 +1,8 @@
+import Logo from './logo'
+export default function Footer() {
+  return (
+    <footer>
+      <Logo />
+    </footer>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
+import Hero from 'components/hero'
+
 export default function Home() {
   return (
-    <>
-      <header>HEADER</header>
-      <main>
-        <div>
-          <h1>CUBE</h1>
-          <p>アウトプットしていくサイト</p>
-        </div>
-      </main>
-      <footer>FOOTER</footer>
-    </>
+    <Hero
+      title="CUBE"
+      subtitle="アウトプットしていくサイト"
+      imageOn
+    />
   )
 }
