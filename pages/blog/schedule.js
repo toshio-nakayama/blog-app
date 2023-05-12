698faeec138eb1c78f1e8270c1abd0814f6424a8
Toshio <toshio.h1990@gmail.com>
2023-05-12 16:51:51 +0900

    feat: Of the data retrieved from the Headless CMS, the title and images were displayed on the page.

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,5 +1,15 @@
 import { getPostBySlug } from 'lib/api'
+import { extractText } from 'lib/extract-text'
+import Meta from 'components/meta'
 import Container from 'components/container'
+import PostHeader from 'components/post-header'
+import PostBody from 'components/post-body'
+import {
+  TwoColumn, TwoColumnMain, TwoColumnSidebar
+} from 'components/two-column'
+import ConvertBody from 'components/convert-body'
+import PostCategories from 'components/post-categories'
+import Image from 'next/legacy/image'
 
 export default function Schedule({
   title,
@@ -10,7 +20,39 @@ export default function Schedule({
 }) {
   return (
     <Container>
-      <h1>{title}</h1>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+      <article>
+        <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt=''
+            layout='responsive'
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes='{min-width: 1152px) 1152px, 100vw'
+            priority
+          />
+        </figure>
+
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={content}/>
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar>
+            <PostCategories categories={categories}/>
+          </TwoColumnSidebar>
+        </TwoColumn>
+      </article>
     </Container>
   )
 }
@@ -19,6 +61,8 @@ export async function getStaticProps() {
   const slug = 'schedule'
   const post = await getPostBySlug(slug)
 
+  const description = extractText(post.content)
+
   return {
     props: {
       title: post.title,
@@ -26,6 +70,7 @@ export async function getStaticProps() {
       content: post.content,
       eyecatch: post.eyecatch,
       categories: post.categories,
+      description: description,
     },
   }
 }
