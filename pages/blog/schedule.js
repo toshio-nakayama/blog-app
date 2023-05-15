b565f59820f17b397e040e31e527ac5dd1d4bc85
Toshio <toshio.h1990@gmail.com>
2023-05-15 17:13:54 +0900

    fix schedule.js

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -5,72 +5,73 @@ import Container from 'components/container'
 import PostHeader from 'components/post-header'
 import PostBody from 'components/post-body'
 import {
-  TwoColumn, TwoColumnMain, TwoColumnSidebar
+    TwoColumn, TwoColumnMain, TwoColumnSidebar
 } from 'components/two-column'
 import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
 import Image from 'next/legacy/image'
 
 export default function Schedule({
-  title,
-  publish,
-  content,
-  eyecatch,
-  categories,
+    title,
+    publish,
+    content,
+    eyecatch,
+    categories,
+    description
 }) {
-  return (
-    <Container>
-      <Meta
-        pageTitle={title}
-        pageDesc={description}
-        pageImg={eyecatch.url}
-        pageImgW={eyecatch.width}
-        pageImgH={eyecatch.height}
-      />
-      <article>
-        <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+    return (
+        <Container>
+            <Meta
+                pageTitle={title}
+                pageDesc={description}
+                pageImg={eyecatch.url}
+                pageImgW={eyecatch.width}
+                pageImgH={eyecatch.height}
+            />
+            <article>
+                <PostHeader title={title} subtitle="Blog Article" publish={publish} />
 
-        <figure>
-          <Image
-            src={eyecatch.url}
-            alt=''
-            layout='responsive'
-            width={eyecatch.width}
-            height={eyecatch.height}
-            sizes='{min-width: 1152px) 1152px, 100vw'
-            priority
-          />
-        </figure>
+                <figure>
+                    <Image
+                        src={eyecatch.url}
+                        alt=''
+                        layout='responsive'
+                        width={eyecatch.width}
+                        height={eyecatch.height}
+                        sizes='{min-width: 1152px) 1152px, 100vw'
+                        priority
+                    />
+                </figure>
 
-        <TwoColumn>
-          <TwoColumnMain>
-            <PostBody>
-              <ConvertBody contentHTML={content}/>
-            </PostBody>
-          </TwoColumnMain>
-          <TwoColumnSidebar>
-            <PostCategories categories={categories}/>
-          </TwoColumnSidebar>
-        </TwoColumn>
-      </article>
-    </Container>
-  )
+                <TwoColumn>
+                    <TwoColumnMain>
+                        <PostBody>
+                            <ConvertBody contentHTML={content} />
+                        </PostBody>
+                    </TwoColumnMain>
+                    <TwoColumnSidebar>
+                        <PostCategories categories={categories} />
+                    </TwoColumnSidebar>
+                </TwoColumn>
+            </article>
+        </Container>
+    )
 }
 
 export async function getStaticProps() {
-  const slug = 'schedule'
-  const post = await getPostBySlug(slug)
+    const slug = 'schedule'
+    const post = await getPostBySlug(slug)
 
-  const description = extractText(post.content)
+    const description = extractText(post.content)
 
-  return {
-    props: {
-      title: post.title,
-      publish: post.publishDate,
-      content: post.content,
-      eyecatch: post.eyecatch,
-      categories: post.categories,
-      description: description,
-    },
-  }
+    return {
+        props: {
+            title: post.title,
+            publish: post.publishDate,
+            content: post.content,
+            eyecatch: post.eyecatch,
+            categories: post.categories,
+            description: description,
+        },
+    }
 }
