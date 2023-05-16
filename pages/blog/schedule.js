79b0c870fd5ff772d4e1e0255590be8b2ff37ec0
Toshio <toshio.h1990@gmail.com>
2023-05-16 16:20:13 +0900

    fix: Provide a local alternate image.

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -5,73 +5,79 @@ import Container from 'components/container'
 import PostHeader from 'components/post-header'
 import PostBody from 'components/post-body'
 import {
-    TwoColumn, TwoColumnMain, TwoColumnSidebar
+  TwoColumn, TwoColumnMain, TwoColumnSidebar
 } from 'components/two-column'
 import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
 import Image from 'next/legacy/image'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 
 export default function Schedule({
-    title,
-    publish,
-    content,
-    eyecatch,
-    categories,
-    description
+  title,
+  publish,
+  content,
+  eyecatch,
+  categories,
+  description
 }) {
-    return (
-        <Container>
-            <Meta
-                pageTitle={title}
-                pageDesc={description}
-                pageImg={eyecatch.url}
-                pageImgW={eyecatch.width}
-                pageImgH={eyecatch.height}
-            />
-            <article>
-                <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+  return (
+    <Container>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+      <article>
+        <PostHeader title={title} subtitle="Blog Article" publish={publish} />
 
-                <figure>
-                    <Image
-                        src={eyecatch.url}
-                        alt=''
-                        layout='responsive'
-                        width={eyecatch.width}
-                        height={eyecatch.height}
-                        sizes='{min-width: 1152px) 1152px, 100vw'
-                        priority
-                    />
-                </figure>
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt=''
+            layout='responsive'
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes='{min-width: 1152px) 1152px, 100vw'
+            priority
+            placeholder='blur'
+            blurDataURL={eyecatch.blurDataURL}
+          />
+        </figure>
 
-                <TwoColumn>
-                    <TwoColumnMain>
-                        <PostBody>
-                            <ConvertBody contentHTML={content} />
-                        </PostBody>
-                    </TwoColumnMain>
-                    <TwoColumnSidebar>
-                        <PostCategories categories={categories} />
-                    </TwoColumnSidebar>
-                </TwoColumn>
-            </article>
-        </Container>
-    )
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={content}/>
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar>
+            <PostCategories categories={categories}/>
+          </TwoColumnSidebar>
+        </TwoColumn>
+      </article>
+    </Container>
+  )
 }
 
 export async function getStaticProps() {
-    const slug = 'schedule'
-    const post = await getPostBySlug(slug)
+  const slug = 'micro'
+  const post = await getPostBySlug(slug)
+  const description = extractText(post.content)
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
 
-    const description = extractText(post.content)
-
-    return {
-        props: {
-            title: post.title,
-            publish: post.publishDate,
-            content: post.content,
-            eyecatch: post.eyecatch,
-            categories: post.categories,
-            description: description,
-        },
-    }
+  return {
+    props: {
+      title: post.title,
+      publish: post.publishDate,
+      content: post.content,
+      eyecatch: eyecatch,
+      categories: post.categories,
+      description: description,
+    },
+  }
 }
